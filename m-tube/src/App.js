0f9862dd7fdce1de576ef2417d23ee9808e59d78
pallishree-saswat <saswataeroarya@gmail.com>
2020-08-12 23:39:41 +0530

    added to github

---
+++ m-tube/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import LoginPage from "./pages/LoginPage";
+import VideoDetail from "./pages/VideoDetail";
+import HomePage from "./pages/HomePage";
+import SearchResultPage from "./pages/SearchResultPage";
+import PlaylistPage from "./pages/PlaylistPage";
+
+import NavBar from "./components/NavBar";
+import Search from "./components/Search";
+
+import "./App.css";
+import ProfilePage from "./pages/ProfilePage";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Search />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/profile" component={ProfilePage} />
+        <Route exact path="/videos/:videoId" component={VideoDetail} />
+        <Route exact path="/playlists" component={PlaylistPage} />
+        <Route exact path="/search/:searchQuery" component={SearchResultPage} />
+        <Redirect to="/" />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
